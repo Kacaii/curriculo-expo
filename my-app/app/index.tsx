1fc03a044a40fdf20f262c95cb04ac7e1209be88
Kacaii <kacaii.privado@gmail.com>
2025-11-04 14:20:14 -0300

    start

---
+++ my-app/app/index.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const lang = ["Typescript", "Javascript", "Golang", "Zig", "Python", "SQL"];
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ textDecorationLine: "underline", marginBottom: 10 }}>
+        Pedro Ayres
+      </Text>
+
+      <Text style={{ fontWeight: "bold" }}>Languages</Text>
+      {lang.map((language, index) => (
+        <ViewLanguage key={index} lang={language} />
+      ))}
+    </View>
+  );
+}
+
+function ViewLanguage({ lang }: { lang: string }) {
+  return <Text>{lang}</Text>;
+}
