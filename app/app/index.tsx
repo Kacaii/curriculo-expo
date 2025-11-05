6dfa6c2158fb25373bb02c321c2e21ab1ab49d6c
Kacaii <kacaii.privado@gmail.com>
2025-11-05 20:33:58 -0300

    add languages and technologies

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,11 +1,28 @@
 import { Text, View, StyleSheet, ScrollView } from "react-native";
 import React from "react";
 
+const fav_languages = ["Typescript", "Golang", "Zig", "Gleam", "SQL"];
+const fav_technologies = ["Git", "Jujutsu", "Beam VM", "PostgreSQL"];
+
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pedro Ayres 🌠 </Text>
       <Text style={styles.subtitle}>Backend Developer</Text>
+
+      <Text style={styles.list_header}>Favorite Languages:</Text>
+      {fav_languages.map((language, index) => (
+        <Text key={index} style={styles.text}>
+          * {language}
+        </Text>
+      ))}
+
+      <Text style={styles.list_header}>Favorite Technologies</Text>
+      {fav_technologies.map((tech, index) => (
+        <Text key={index} style={styles.text}>
+          * {tech}
+        </Text>
+      ))}
     </View>
   );
 }
@@ -17,9 +34,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  title: { fontWeight: "bold", color: "#cdd6f4" },
 
+  title: { fontWeight: "bold", color: "#cdd6f4" },
   subtitle: { textDecorationStyle: "dashed", color: "#cdd6f4" },
+
+  list_header: {
+    fontWeight: "bold",
+    color: "#cdd6f4",
+    marginVertical: 10,
+  },
+
   bio: {},
-  text: {},
+  text: { color: "#cdd6f4" },
 });
